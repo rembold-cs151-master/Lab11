594310a216dd9e425cd2fb4dc24f8813c0bfc7f2
Jed Rembold <jedediah.r@gmail.com>
2020-09-28 12:29:54 -0700

    Add initial materials

---
+++ Lab11.py
@@ -0,0 +1,19 @@
+# File: Lab11.py
+
+"""
+Program to count the number of English words that begin
+with each letter of the alphabet and display that
+information to the screen in a nicely formatted manner.
+"""
+
+# I included english.py here in the repository, but you'll need to import it
+
+
+
+# Then include your function or code to count and output the desired information
+# to the screen here below. You are just outputting information to the screen
+# today, no need to have any function you write return anything
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Lab11
-Starting template for Lab 11
+# Lab 11
+
+The purpose of today's lab is to get some practice with the `english` library and to practice using string methods and our new string formatting techniques. Full instructions are on the webpage.
+
+There will likely not be autotesting for this.
